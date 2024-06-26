b449dc1291dccc1a15d8eaabb7930a20d2d25767
souravsingha13 <souravsingha1304021@gmail.com>
2024-06-27 02:46:47 +0600

    :sparkles: login page

--- app/login/page.js
+++ app/login/page.js
@@ -1,7 +1,23 @@
-import React from "react";
+"use client"
+import * as React from 'react';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
 
-export default function About(){
-    return(
-        <div>This is for user login</div>
+export default function About() {
+    const [value, setValue] = React.useState(2);
+
+    const handleChange = (event, newValue) => {
+        console.log(newValue)
+        setValue(newValue);
+    };
+    return (
+        <div>
+            <Tabs value={value} onChange={handleChange} aria-label="tabs example">
+                <Tab label="Login" />
+                <Tab label="Registration" />
+            </Tabs>
+            {value === 0 && <div>Hi, this is Login</div>}
+            {value === 1 && <div>This is Registration</div>}
+        </div>
     )
 }
